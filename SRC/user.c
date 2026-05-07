27c46d93cf63e165a28038b55d88f650c69a0d54
tomascacoilo <tomascacoilo2006@hotmail.com>
2026-05-07 12:00:00 +0100

    validei tudo da struct pessoa

--- SRC/user.c
+++ SRC/user.c
@@ -1,19 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "DATA.h"
+
 
 typedef struct {
     char* nome;
     data nasc;
-    char* curso;
+    int curso;
     int ano;
     int numero;
     float saldo;
 }person;
 
 
-int valida(person pessoa){
-    if(pessoa.nasc.ano >= pessoa.ano  || pessoa.numero <0  || pessoa.saldo<0) return 0;
-        
-  
-    
+
+
+int valida_pessoa(person* pessoa){
+    if(pessoa->nasc.ano >= pessoa->ano){  //valida se a pessoa está na universidade antes de nascer
+        return 0;
+    } 
+
+    if(pessoa->numero <0){  //verifica que não existem números negativos
+        return 0;
+    }  
+
+    if(pessoa->saldo<0) { //verifica que o saldo não é um número negativo
+        return 0;
+    }
+
+    if(pessoa->curso <1 || pessoa->curso>5){  //vê se o "curso" inserido está válido
+        return 0;
+    }
+
+    return 1;  
 }
