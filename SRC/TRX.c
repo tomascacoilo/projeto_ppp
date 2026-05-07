9cb22d71a5d62d61a3854ddb8208f2c6ce7887b1
tomasmarcoss <tomasmarcos2006@gmail.com>
2026-05-07 12:51:33 +0100

    Criei efetuar_despesa e struct noTrans. Nota importante sobre a organização das despesas.

--- SRC/TRX.c
+++ SRC/TRX.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "DATA.h"
 
 typedef struct {
@@ -7,3 +8,28 @@ typedef struct {
     char* info;
     data date;
 } trans;
+
+typedef struct noTrans {
+    trans trx;
+    struct noTrans* next;
+} node_trans;
+
+void efetuar_despesa(node_trans** lista, int valor, char* info, data dt){
+
+    node_trans* novo = (node_trans*) malloc(sizeof(node_trans));
+    if (novo == NULL) {
+        printf("Erro na alocação de memória.\n");
+        return;
+    }
+
+    novo->trx.valor = valor;
+
+    novo->trx.info = (char*) malloc(strlen(info) + 1); // strlen lê td à exceçãp do \0 ----- +1 para incluir o \0
+    strcpy(novo->trx.info, info);
+    
+    novo->trx.date = dt;
+
+}
+
+
+// NOTA: CADA ALUNO DEVERÁ TER UMA LISTA PROPRIA COM AS SUAS DESPESAS
