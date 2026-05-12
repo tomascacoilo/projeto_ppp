7cf33a3ad3eb1ce0e39a1d47e920cf1f24613932
tomascacoilo <tomascacoilo2006@hotmail.com>
2026-05-12 12:25:48 +0100

    fiz a maior parte das funcoes lista, falta apenas acabar a inserir

--- SRC/user.c
+++ SRC/user.c
@@ -41,12 +41,12 @@ typedef struct noLista{  //criar o no
     struct noLista* prox;
 }noLista;
 
-typedef noLista* pLista;
+typedef noLista* pLista; 
 
 //criar a lista
 
 pLista cria(){
-    pLista aux;   //vai ser um ponteiro para um nó´
+    pLista aux;   //vai ser um ponteiro para um nó
     person p1={"",{0,0,0},0,0,0,0.0}; //header
     aux=(pLista)malloc(sizeof(noLista));
     if(aux!= NULL){
@@ -58,9 +58,59 @@ pLista cria(){
 }
 
 // ver se a lista está vazia
-//...
+int vazia(pLista lista){
+    if(lista->prox==NULL){
+        return 1;
+    }
+    return 0;
+}
+
+//inserir um novo aluno
+void insere(pLista lista, person p1){
+    pLista no,ant,inutil;
+    no=(pLista)malloc(sizeof(noLista));
+    if(no!=NULL){
+        no->pessoaLista=p1;
+        procura(lista,p1.numero,)
+    }
+}
+
+
+
+
+//destruir a lista
+pLista destroi(pLista lista){
+    pLista temp_str; //ponteiro temporário para um nó da lista
+    while (!vazia(lista)){
+        temp_str=lista;
+        lista=lista->prox;
+        free(temp_str);
+    }
+    free(lista);
+    return NULL;
+}
+
+
+//procurar um elemento
+void procura(pLista lista, int chave,pLista *ant,pLista *atual){ //procuramos pelo numero do estudante, ou seja, chave é um número
+    *ant=lista;  //aponta para um no
+    *atual=lista->prox;  //e este aponta para o próximo 
+    while((*atual)!=NULL && (*atual)->pessoaLista.numero< chave){
+        *ant=*atual;
+        *atual=(*atual)->prox;
+    }
+    if((*atual)!=NULL && (*atual)->pessoaLista.numero !=chave){
+        *atual=NULL; //se não encontrarmos o elemento 
+    }
+}
+
 
 //eliminar um elemento 
-pLista elimina(){
-    pLista temp_ptr;
+pLista elimina(pLista lista, int chave){
+    pLista ant,atual; //vao ser um ponteiro para um nó
+    procura(lista,chave,&ant,&atual);
+    if(atual != NULL){
+        ant->prox=atual->prox;
+        free(atual);
+    }
 }
