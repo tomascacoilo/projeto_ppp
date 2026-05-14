d830cfc6301e5482d4a6e102f49ccb19f64152ba
tomasmarcoss <tomasmarcos2006@gmail.com>
2026-05-14 12:21:38 +0100

    Lista despesas e as suas funções

--- SRC/TRX.c
+++ SRC/TRX.c
@@ -14,7 +14,41 @@ typedef struct noTrans {
     struct noTrans* next;
 } node_trans;
 
-void efetuar_despesa(node_trans** lista, int valor, char* info, data dt){
+//____________________________LISTA_E_AS_SUAS_FUNÇÕES________________________________
+
+node_trans* create(){
+
+    node_trans* aux;
+    trans trx = {0, NULL, {0,0,0}};
+    aux = (node_trans*) malloc(sizeof(node_trans));
+    if(aux != NULL){
+        aux->trx = trx;
+        aux->next=NULL;
+    }
+
+    return aux;
+}
+
+int check_empty (node_trans* list_trx) { 
+    return list_trx->next == NULL; //1 se tiver vazia, 0 se não tiver
+}
+
+node_trans* clean(node_trans* list_trx) {
+    while (list_trx != NULL) {
+        node_trans *temp_ptr = list_trx;
+        list_trx = list_trx->next;
+
+        if (temp_ptr->trx.info != NULL) { //liberta tbm a memoria da string trx.info
+                free(temp_ptr->trx.info);
+            }
+        
+        free(temp_ptr);
+    }
+  return NULL;
+}
+
+void efetuar_despesa(node_trans* lista, int valor, char* info, data dt){
+    if (lista==NULL) return;
 
     node_trans* novo = (node_trans*) malloc(sizeof(node_trans));
     if (novo == NULL) {
@@ -23,13 +57,14 @@ void efetuar_despesa(node_trans** lista, int valor, char* info, data dt){
     }
 
     novo->trx.valor = valor;
-
     novo->trx.info = (char*) malloc(strlen(info) + 1); // strlen lê td à exceçãp do \0 ----- +1 para incluir o \0
     strcpy(novo->trx.info, info);
-    
     novo->trx.date = dt;
 
+    novo->next = lista->next;
+    lista->next=novo;
 }
+
 
 
 // NOTA: CADA ALUNO DEVERÁ TER UMA LISTA PROPRIA COM AS SUAS DESPESAS
